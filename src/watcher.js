8774bade25a6c2243f0a0a481c611e2a4caf6aba
xiongleixin <leilei@yscredit.com>
2021-09-06 16:16:31 +0800

    fix: 注释

--- src/watcher.js
+++ src/watcher.js
@@ -1,6 +1,6 @@
 import { popTarget, pushTarget } from "./dep.js";
 // options中lazy默认为false 当不是计算属性时 初始化会去收集依赖
-// 当为计算属性时 lazy为true 初始化会走evalute函数 执行computed属性中对应的内容
+// 当为计算属性时 lazy为true 初始化会走evaluate函数 执行computed属性中对应的内容
 // computed中如果有依赖响应式数据 那么就会触发get拦截 便会将当前的watcher收集到对应响应式数据dep的依赖中
 export default function Watcher(cb, options = {}, vm = null) {
     this._cb = cb;
