b29100b1aebfc0b8593b2077ac6588db3d1e161f
codeleilei <972761675@qq.com>
2021-09-13 20:44:07 +0800

    add: comment

--- src/common.js
+++ src/common.js
@@ -1,11 +1,24 @@
+/**
+ * 判断是否为对象
+ * @param {Object} data 
+ * @returns 
+ */
 export function isObject(data) {
     return typeof data == "object" && data != null;
 }
-
+/**
+ * 判断是否为数组
+ * @param {Object} data 
+ * @returns 
+ */
 export function isArray(data) {
     return Array.isArray(data);
 }
-
+/**
+ * 判断是否为data数据格式
+ * @param {Object} data 
+ * @returns 
+ */
 export function isData(data) {
     if(typeof data === "function" || typeof data === 'object') {
         return data !== null;
